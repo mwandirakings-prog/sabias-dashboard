6856fc35e2ded3b78a880a716d8abc9138cfb2ef
Kings Mwandira <mwandirakings@gmail.com>
2026-05-24 15:23:23 +0100

    Fix service worker causing blank white screen

--- public/sw.js
+++ public/sw.js
@@ -1,40 +1,25 @@
-const CACHE_NAME = 'sabias-v1';
-
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-];
+const CACHE_NAME = 'sabias-v2';
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
   self.skipWaiting();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) return response;
-      return fetch(event.request).catch(() => {
-        return caches.match('/index.html');
-      });
-    })
-  );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames =>
       Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
+        cacheNames.map(name => caches.delete(name))
       )
     )
   );
   self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match('/index.html');
+    })
+  );
 });
